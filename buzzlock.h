031d44557a2d821afc0a2e9fb39942025afe5b55
Dylan Garrett <dylan.garrett@gmail.com>
2012-03-11 03:01:48 -0400

    Added userspace data structures

--- buzzlock.h
+++ buzzlock.h
@@ -2,11 +2,31 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <pthread.h>
 
 #define BZZ_BLACK 0
 #define BZZ_GOLD 1
 
+#ifdef BZZ_KERNEL_MODE
+// TODO: kernel mode structures
 typedef uint32_t bzz_t;
+
+#else
+typedef struct _bzz_thread {
+	pthread_cond_t cond;
+	int color;
+	suseconds_t time_created;
+	pid_t tid;
+	struct _bzz_thread* next;
+} bzz_thread;
+
+typedef struct {
+	bzz_thread* yellow_threads;
+	bzz_thread* black_threads;
+	bzz_thread* current_locked;
+	pthread_mutex_t mutex;
+} bzz_t;
+#endif
 
 void init_bzz(bzz_t *, int, useconds_t);
 void bzz_color(int, bzz_t *);
--- usrbuzzlock.c
+++ usrbuzzlock.c
@@ -3,23 +3,30 @@
 void init_bzz(bzz_t *lock, int num_threads, useconds_t timeout)
 {
 	printf("init_bzz: %d %d %d\n", *lock, num_threads, timeout);
+
+	// allocate memory
 }
 
 void bzz_color(int color, bzz_t *lock)
 {
-	if (!color)
+	
+	if (color == BZZ_BLACK) {
 		printf("bzz_color: BLACK %d\n", *lock);
-	else
+	} else if (color == BZZ_GOLD) {
 		printf("bzz_color: GOLD %d\n", *lock);
+	}
 }
 
 void bzz_lock(bzz_t *lock)
 {
+	// TODO: wait on bzz_thread's condition variable
+	// TODO: needs to happen differently if nothing has the lock
 	printf("bzz_lock: %d\n", *lock);
 }
 
 void bzz_release(bzz_t *lock)
 {
+	// TODO: find next thread to unlock and signal its condition
 	printf("bzz_release: %d\n", *lock);
 }
 
