88e8888c6317d97e3e178c838c6c22ba6ff81b61
Dylan Garrett <dylan.garrett@gmail.com>
2012-03-14 03:12:43 -0400

    Fix issue with locks arriving with no already enqueued locks

--- usrbuzzlock.c
+++ usrbuzzlock.c
@@ -191,6 +191,7 @@ void bzz_lock(bzz_t *lock)
 
 	// Not currently locked
 	if (lock->current_locked == NULL) {
+		queue_thread(lock, to_lock, 0);
 		lock->current_locked = to_lock;
 	} else {
 		queue_thread(lock, to_lock);
